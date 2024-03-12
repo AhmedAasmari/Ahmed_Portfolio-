6617de7e62e642ff672be32895a571472ebf2403
Ahmed <162879375+AhmedAasmari@users.noreply.github.com>
2024-03-12 11:59:31 +0000

    sqlalchemy and bootstrap

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine,text
+engine = create_engine("mysql+pymysql://admin:1234admin@database-1.cxayace4c8yj.us-east-1.rds.amazonaws.com/projects?charset=utf8mb4")
+
+with engine.connect() as conn:
+  result = conn.execute(text("select * from trial"))
+  print(result.all())
+#interaction between mysql and python
+#1. create a database
